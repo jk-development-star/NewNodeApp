a9ce936246784b3060d42808d0d6281b1d6bab47
jk-development-stargit config --global user.name jk-development-stargit config --global user.name jk-development-star <vishwakarmajeetu137@gmail.com>
2023-06-17 17:28:20 +0530

    EJS and Js file changes

--- app.js
+++ app.js
@@ -8,27 +8,31 @@ const PORT = process.env.PORT || 8082;
 const path = require("path");
 const bodyParser = require("body-parser");
 const expressLayouts = require("express-ejs-layouts");
-const expressSession = require("express-session");
+const session = require("express-session");
+const checkJWTAuth = require("./middlewares/jwtAuth");
 const flash = require("express-flash");
 const cookieParser = require("cookie-parser");
+
 const app = express();
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cookieParser());
+
 // set the template engine
 app.set("view engine", "ejs");
 app.use(expressLayouts);
-// set the public path
+
+// set the public folder path
 app.use(express.static(path.join(__dirname, "public")));
 app.set("layout extractScripts", true);
 app.set("layout extractStyles", true);
 
+// Cookie, flash and session
+app.use(cookieParser());
 app.use(flash());
-
 app.use(
-  expressSession({
+  session({
     secret: "secret string",
     resave: false,
     saveUninitialized: false,
@@ -36,13 +40,37 @@ app.use(
   })
 );
 
+// Making logged in user details global
 app.use(function (req, res, next) {
   res.locals.user = req.cookies.auth;
   next();
 });
+
+// Logout the user
+app.post("/logout", checkJWTAuth, function (req, res) {
+  try {
+    if (req.session) {
+      req.session.destroy();
+      res.clearCookie("auth"); // clean up!
+      return res.redirect("/");
+    } else {
+      return res.redirect("back");
+    }
+  } catch (error) {
+    req.flash("error", error);
+    return res.redirect("back");
+  }
+});
+
+//Routes
 app.use(require("./routes/authroutes"));
 app.use(require("./routes/user.routes"));
 app.use(require("./routes/lead.routes"));
+app.use(require("./routes/calendar.routes"));
+app.use(require("./routes/tasks.routes"));
+app.use(require("./routes/actionitems.routes"));
+app.use(require("./routes/actionItemsTasks.routes"));
+
 // connect database
 connectDB(DATABASE_URL);
 
